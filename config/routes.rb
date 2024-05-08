9022a464c4233105f7d9e9b2fba8c3e9cc7b3c78
Nghinn <ngo.thao.huong@sun-asterisk.com>
2024-05-08 15:59:27 +0700

    static_pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+  scope "(:locale)", locale: /en|vi/ do
+  # Ex:- scope :active, -> {where(:active => true)}
+  get 'static_pages/home'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  end
 end
